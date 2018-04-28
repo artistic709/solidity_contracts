74d6947472e6712ff8b2720d4bd1686e6a54ee97
artistic709 <artistic709@gmail.com>
2018-04-28 20:00:10 +0800

    Create proxy.sol

---
+++ proxy.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.20;
+
+contract proxy {
+
+	address public owner;
+	
+	function proxy(){
+		owner = msg.sender;
+	}
+
+	modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function execute(address to,uint value,bytes data) public onlyOwner{
+  	to.call.value(value)(data);
+  }
+  
+  function ()public payable{
+  }
+
+}
