53c1d0125f28a0b8097c0aff199afbb9e6c81553
artistic709 <artistic709@gmail.com>
2018-06-16 13:49:21 +0800

    Update cryptocow.sol

--- cryptocow.sol
+++ cryptocow.sol
@@ -33,7 +33,6 @@ library SafeMath {
 contract Ownable {
   address public owner;
 
-
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
@@ -55,10 +54,9 @@ contract Ownable {
    */
   function transferOwnership(address newOwner) public onlyOwner {
     require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
+    emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
-
 }
 
 /**
@@ -66,6 +64,9 @@ contract Ownable {
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20Interface {
+  string public name;
+  string public symbol;
+  uint8 public decimals;
   uint256 public totalSupply;
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
@@ -76,55 +77,37 @@ contract ERC20Interface {
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract DetailedERC20 is ERC20Interface {
-  string public name;
-  string public symbol;
-  uint8 public decimals;
-
-  function DetailedERC20(string _name, string _symbol, uint8 _decimals) public {
-    name = _name;
-    symbol = _symbol;
-    decimals = _decimals;
-  }
-}
-
-contract ERC20Token is Ownable, DetailedERC20 {
+contract ERC20Token is Ownable, ERC20Interface {
   using SafeMath for uint256;
 
   mapping(address => uint256) internal balances;
   mapping(address => mapping(address => uint256)) internal allowed;
 
-  // ERC20Basic methods
   function balanceOf(address _owner) public view returns (uint256){
     return balances[_owner];
   }
   function _transfer(address _from,address _to,uint256 _value)internal returns(bool){
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
-    Transfer(_from, _to, _value);
+    emit Transfer(_from, _to, _value);
     return true;
   }
   function transfer(address _to, uint256 _value) public returns (bool) {
     return _transfer(msg.sender,_to,_value);
   }
 
-  // ERC20 3 methods
   function allowance(address _owner, address _spender) public view returns (uint256) {
     return allowed[_owner][_spender];
   }
 
   function approve(address _spender, uint256 _value) public returns (bool) {
     allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
+    emit Approval(msg.sender, _spender, _value);
     return true;
   }
 
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
     require(msg.sender != _from);
-
-    if (allowed[_from][msg.sender] < _value) {
-      return false;
-    }
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
     return _transfer(_from,_to,_value);
   }
@@ -135,26 +118,40 @@ contract CryptoCow is ERC20Token{
 
   uint256 constant PSN=1642182;
   uint256 constant PSNH=821091;
-  bool public initialized=false;
   address public ceoAddress;
   uint256 public marketEggs;
-  function CryptoCow() public payable{
-    //
+  function CryptoCow(uint256 initialSupply,string _name, string _symbol, uint8 _decimals) public payable{
+    totalSupply = initialSupply;
+    balances[0xbeef] = initialSupply;
+    name = _name;
+    symbol = _symbol;
+    decimals = _decimals;
   }
+  function ()public payable{
+    buyToken();
+  }
+  event Award(address awardee,uint256 amount);
+
+  function award(address _awardee,uint256 _amount)public onlyOwner{
+    totalSupply=totalSupply.add(_amount);
+    balances[_awardee]=balances[_awardee].add(_amount);
+    emit Award(_awardee,_amount);
+  }
+
   function selltoken(uint256 _amount) public{
     uint256 tokenValue = calculateTokenSell(_amount);
-    balances[msg.sender]=balances[msg.sender].sub(_amount);
-    balances[0xbeef]=balances[0xbeef].add(_amount);
+    _transfer(msg.sender,0xbeef,_amount);
     msg.sender.transfer(tokenValue);
   }
+
   function buyToken() public payable{
     uint256 tokenBought=calculateTokenBuy(msg.value,SafeMath.sub(this.balance,msg.value));
-    balances[0xbeef]=balances[0xbeef].sub(tokenBought);
-    balances[msg.sender]=balances[msg.sender].add(tokenBought);
+    _transfer(0xbeef,msg.sender,tokenBought);
   }
-  //magic trade balancing algorithm
-  function calculateTrade(uint256 rt,uint256 rs, uint256 bs) public view returns(uint256){
+  //model stoled from EtherShrimpFarm
+  function calculateTrade(uint256 rt,uint256 rs, uint256 bs) public pure returns(uint256){
     //(PSN*bs)/(PSNH+((PSN*rs+PSNH*rt)/rt));
+    //PSN.mul(bs).div(PSNH.add(PSN.mul(rs).add(PSNH.mul(rt)).div(rt)))
     return SafeMath.div(SafeMath.mul(PSN,bs),SafeMath.add(PSNH,SafeMath.div(SafeMath.add(SafeMath.mul(PSN,rs),SafeMath.mul(PSNH,rt)),rt)));
   }
   function calculateTokenSell(uint256 eggs) public view returns(uint256){
@@ -164,15 +161,9 @@ contract CryptoCow is ERC20Token{
     return calculateTrade(eth,contractBalance,balances[0xbeef]);
   }
   function calculateTokenBuySimple(uint256 eth) public view returns(uint256){
-      return calculateTokenBuy(eth,this.balance);
-  }
-  function seedMarket(uint256 eggs) public payable{
-      require(marketEggs==0);
-      initialized=true;
-      marketEggs=eggs;
+    return calculateTokenBuy(eth,this.balance);
   }
   function getBalance() public view returns(uint256){
-      return this.balance;
+    return this.balance;
   }
-
-
+}
